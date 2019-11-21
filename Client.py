17dc78f1510847db20c1ee46652c799f89cd9b3b
Eagle47 <egor.vorontcov@mail.ru>
2019-11-21 23:51:31 +0300

    Add def make_a_move

--- Client.py
+++ Client.py
@@ -1,15 +1,38 @@
-from Core import create_maze
+from Core import create_maze, make_a_move
 
 f = open('input.txt', 'r')
+print('Enter number of nodes: ')
 number_of_nodes = int(f.readline())
 
 list_of_edges = []
 line = f.readline()
-while line != '':
+print('Enter edges: ')
+while line != 'start\n':
     tmp = list(int(x) - 1 for x in line.split())
     list_of_edges.append([tuple(tmp[0:2]), tuple(tmp[2:])])
     line = f.readline()
 
 maze = create_maze(number_of_nodes, list_of_edges)
+print('True maze created')
 print('Num of nodes: ', maze.number_of_nodes())
 print('Num of edges: ', maze.number_of_edges())
+
+l_player = []
+player_maze = create_maze(1, l_player)
+print('\nMaze for player created')
+print('Num of nodes: ', player_maze.number_of_nodes())
+print('Num of edges: ', player_maze.number_of_edges())
+
+print('\nEnter start position: ')
+curr_pos = tuple(int(x) for x in f.readline().split())
+print(curr_pos)
+player_maze.add_node(curr_pos)
+
+while True:
+    new_pos = tuple(int(x) for x in input().split())
+    curr_pos = make_a_move(maze, player_maze, new_pos, curr_pos)
+    print('New pos: ', curr_pos)
+    print('Num of edges: ', player_maze.number_of_edges())
+    print('Num of nodes: ', player_maze.number_of_nodes())
+
+f.close()
--- Core.py
+++ Core.py
@@ -4,11 +4,24 @@ import networkx as nx
 def create_maze(nodes, edges):
     maze = nx.Graph()
 
-    for i in range(nodes):
-        for j in range(nodes):
-            maze.add_node((i, j))
+    if nodes > 1:
+        for i in range(nodes):
+            for j in range(nodes):
+                maze.add_node((i, j))
 
     for edge in edges:
         maze.add_edge(edge[0], edge[1])
 
     return maze
+
+
+def make_a_move(true_maze, player_maze, next_pos, curr_pos):
+    if not true_maze.has_edge(curr_pos, next_pos):
+        print('WALL')
+        return curr_pos
+
+    player_maze.add_node(next_pos)
+    player_maze.add_edge(curr_pos, next_pos)
+    new_pos = next_pos
+
+    return new_pos
