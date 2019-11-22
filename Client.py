081017d55606b72dad89d64adc899551ab978660
Eagle47 <egor.vorontcov@mail.ru>
2019-11-22 15:47:06 +0300

    Add def is_over. Improve def make_a_move.

--- Client.py
+++ Client.py
@@ -1,4 +1,4 @@
-from Core import create_maze, make_a_move
+from Core import create_maze, make_a_move, is_over
 
 f = open('input.txt', 'r')
 print('Enter number of nodes: ')
@@ -8,7 +8,7 @@ list_of_edges = []
 line = f.readline()
 print('Enter edges: ')
 while line != 'start\n':
-    tmp = list(int(x) - 1 for x in line.split())
+    tmp = list(int(x) for x in line.split())
     list_of_edges.append([tuple(tmp[0:2]), tuple(tmp[2:])])
     line = f.readline()
 
@@ -25,14 +25,23 @@ print('Num of edges: ', player_maze.number_of_edges())
 
 print('\nEnter start position: ')
 curr_pos = tuple(int(x) for x in f.readline().split())
-print(curr_pos)
 player_maze.add_node(curr_pos)
+print(curr_pos)
 
-while True:
+print('\nEnter exit edge: ')
+end = list(int(x) for x in f.readline().split())
+maze.add_edge(tuple(end[0:2]), tuple(end[2:]))
+print(tuple(end[0:2]), tuple(end[2:]))
+print('Num of nodes: ', maze.number_of_nodes())
+print('Num of edges: ', maze.number_of_edges())
+
+print('Your position: ', curr_pos)
+while not is_over(curr_pos, tuple(end[2:])):
+    print('\nEnter new position: ')
     new_pos = tuple(int(x) for x in input().split())
     curr_pos = make_a_move(maze, player_maze, new_pos, curr_pos)
     print('New pos: ', curr_pos)
-    print('Num of edges: ', player_maze.number_of_edges())
     print('Num of nodes: ', player_maze.number_of_nodes())
+    print('Num of edges: ', player_maze.number_of_edges())
 
 f.close()
--- Core.py
+++ Core.py
@@ -5,8 +5,8 @@ def create_maze(nodes, edges):
     maze = nx.Graph()
 
     if nodes > 1:
-        for i in range(nodes):
-            for j in range(nodes):
+        for i in range(1, nodes + 1):
+            for j in range(1, nodes + 1):
                 maze.add_node((i, j))
 
     for edge in edges:
@@ -16,6 +16,9 @@ def create_maze(nodes, edges):
 
 
 def make_a_move(true_maze, player_maze, next_pos, curr_pos):
+    if true_maze.has_node(next_pos):
+        player_maze.add_node(next_pos)
+
     if not true_maze.has_edge(curr_pos, next_pos):
         print('WALL')
         return curr_pos
@@ -25,3 +28,10 @@ def make_a_move(true_maze, player_maze, next_pos, curr_pos):
     new_pos = next_pos
 
     return new_pos
+
+
+def is_over(curr_pos, end):
+    if curr_pos == end:
+        print('You won!')
+        return True
+    return False
